df902e712cbcc66464b8c3550741af3a69183db6
Lara Lyvia <lara.lyvia@academico.ifpb.edu.br>
2025-05-06 11:25:47 -0300

    Texto que representa as mudanças realizadas

--- app/index.tsx
+++ app/index.tsx
@@ -1,22 +1,49 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
+const usuarios = [
+  {
+    id: 1,
+    nome: "Roberto",
+    cargo: "Professor"
+  },
+  {
+    id: 2,
+    nome: "Erika",
+    cargo: "Aluna"
+  }
+  ]; 
+  
+  export default function Index() {
   return (
     <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
+      style={style.container}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    {
+      usuarios.map( item => (
+
+        <Text style={style.textoLista}>(item.nome)</Text>
+      ))
+    }
+     </View>
   );
 }
 
 const style = StyleSheet.create({
+  container: {
   flex: 1,
   justifyContent: "center",
-  aligniltems: "center",
+  alignItems: "center",
+  },
+
+  itemLista: {
+    width: '1001',
+    padding: 10,
+  }
+  
+  textoLista: {
+    fontSize: 20
+  }
 
 })
+
+
